2f4d1e52a2309ee32919e423cd5e439abfccc378
Lino3D <kamiskijakub@gmail.com>
2015-01-14 23:40:33 +0100

    Working on filmpage layout.

--- FilmBayMVC/Controllers/FilmPageController.cs
+++ FilmBayMVC/Controllers/FilmPageController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using FilmBayMVC.Models;
 using FilmBayMVC;
 using System.Threading.Tasks;
+using FilmBayMVC.Connectivity;
+
 namespace FilmBayMVC.Controllers
 {
     public class FilmPageController : Controller
@@ -19,35 +21,9 @@ namespace FilmBayMVC.Controllers
         public async Task<ActionResult> FilmPage()
         {
             int id =6;
-            film_table f = new film_table();
 
-            f = await DBAccess.GetFilmById(id);
-            List<writers_table> writers = await DBAccess.GetWriters(id);
-            List<producer_table> producers = await DBAccess.GetProducers(id);
-            List<music_creator_table> composers = await DBAccess.GetComposers(id);
-            List<actor_table> actors = await DBAccess.GetActors(id);
-            List<string> languages = await DBAccess.GetLanguages(id);
-            List<string> genres = await DBAccess.GetGenres(id);
-
-            FilmPageModel film = new FilmPageModel();
-            film.Director = f.director_name + "" + f.director_surname;
-            film.poster = f.poster_url;
-            film.storyline = f.storyline;
-            film.Title = f.title;
-            film.rating = f.rating.ToString();
-            film.duration = f.duration.ToString();
-            
-            foreach(writers_table w in writers)
-            {
-                film.Writers.Add(w.ToString());
-            }
-            foreach (producer_table p in producers)
-            {
-                film.Producers.Add(p.ToString());
-            }
-            film.actors = actors;
-            film.Composers = composers;
-            film.ReleaseDate = f.release_date.ToString().Substring(0, 10);
+            FilmPageModel film = await ModelCreator.getFilmPageModel(id);
+       
 
             return View(film);
         }
