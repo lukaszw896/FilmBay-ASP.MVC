36a7f40efaa2d1b65b9c17e556f6c3e61bb044b1
lukaszw896 <lukaszw896@gmail.com>
2015-01-14 17:25:43 +0100

    things

--- FilmBayMVC/Controllers/HomeController.cs
+++ FilmBayMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FilmBayMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,55 @@ namespace FilmBayMVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpGet]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+        public ActionResult SearchFilmResult(String filmName)
+        {
+            var list = new List<MovieSearchReturnObjectViewModel>();
+            if (filmName != null)
+            {
+
+
+                List<MovieSearchReturnObject> tmpList = null;
+                tmpList= DBAccess.SearchedByTitle
+                //tmpList = TMDbApi.movieSearch(filmName);
+                /*
+                 * 
+                 * DAŁEM SORTOWANIE POPULARNOŚCI ZA POMOCĄ BUBBLE SORTA. TRZEBA ZMIENIĆ!!!!!
+                 * 
+                 * */
+                MovieSearchReturnObject tmp;
+                for (int i = 0; i < tmpList.Count(); i++)
+                {
+                    for (int j = 0; j < tmpList.Count() - 1; j++)
+                    {
+                        if (tmpList[j].popularity < tmpList[j + 1].popularity)
+                        {
+                            tmp = tmpList[j];
+                            tmpList[j] = tmpList[j + 1];
+                            tmpList[j + 1] = tmp;
+                        }
+                    }
+
+                }
+                for (int i = 0; i < tmpList.Count(); i++)
+                {
+                    MovieSearchReturnObjectViewModel tmpModel = new MovieSearchReturnObjectViewModel();
+                    tmpModel.id = tmpList[i].id;
+                    tmpModel.orginalTitle = tmpList[i].orginalTitle;
+                    tmpModel.popularity = tmpList[i].popularity;
+                    tmpModel.posterPath = tmpList[i].posterPath;
+                    tmpModel.releaseDate = tmpList[i].releaseDate;
+                    tmpModel.title = tmpList[i].title;
+
+                    list.Add(tmpModel);
+                }
+
+                return PartialView("_SearchResultPartial", list);
+            }
+            else
+                return PartialView("_SearchResultPartial");
         }
     }
 }
