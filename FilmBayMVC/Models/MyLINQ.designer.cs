f6bae56ef856933c1a59de18e2979e32b369bd5a
Lino3D <kamiskijakub@gmail.com>
2015-01-14 15:19:04 +0100

    Little here and there

--- FilmBayMVC/Models/MyLINQ.designer.cs
+++ FilmBayMVC/Models/MyLINQ.designer.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FilmBayMVC
+namespace FilmBayMVC.Models
 {
 	using System.Data.Linq;
 	using System.Data.Linq.Mapping;
